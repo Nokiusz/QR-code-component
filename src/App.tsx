2b9d52d4541ef0e0671dee5e87d89cd8d24ccda3
Mateusz Kłakus <mati.klakus@gmail.com>
2023-03-11 20:34:02 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import QRCard from './components/QRCard';
+
+function App() {
+
+  return (
+    <div className="App">
+      <QRCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QRCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QRCard = () => {
+  const test:any = 'test';
+    
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default QRCard;
